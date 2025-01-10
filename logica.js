4bda2f525147929b8efa58fc523f01aac0b64baf
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-01-10 14:09:11 -0300

    Atualizar o js

---
+++ logica.js
@@ -0,0 +1,22 @@
+/*Desafios lógica JS Alura
+1. Mostre um alerta com a mensagem "Boas vindas ao nosso site". )*/
+
+alert("Bem vindo(a) ao nosso site");
+
+/*2. Declare uma variável chamada nome
+e atribua a ela o valor Lua*/
+
+let nome = "Lua";
+
+/*3. Crie uma variável chamada idade 
+e atribua a ela o valor de 25*/
+
+let idade = 25;
+
+/*4. Defina uma variável numeroVendas e atribua a ela o valor 50*/
+
+let numeroVendas = 50;
+
+/*5. Defina uma variável saldoDisponivel e atribua a ela o valor de 1000*/
+
+let saldoDisponivel = 1000;
