64c8c393443b435f699a13d7b8b7fe1b74febf30
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-02-06 22:25:32 +0000

    Atualizar o

---
+++ casting.js
@@ -0,0 +1,5 @@
+var variavel11 = prompt('Digite um número');
+var variavel12 = prompt('Digite outro número');
+variavel11 = parseInt(variavel11);
+variavel12 = parseInt(variavel12);
+alert(variavel11 + variavel12);
